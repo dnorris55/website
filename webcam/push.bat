75b86ceb473989a6312970dd0c247ef5c07bd5ee
David <drdave57@gmail.com>
2022-06-30 11:43:25 -0400

    upload webcam photos

--- webcam/push.bat
+++ webcam/push.bat
@@ -1,9 +1,12 @@
 D:
-cd \GitHub\website\webcam
+cd \GitHub\website\webcam\photos
 
-git add webcam.jpg
+git add webcam22.jpg
+git add webcam26.jpg
+git add webcam27.jpg
+git add webcam32.jpg
 
-git commit -am "upload new webcam.jpg"
+git commit -am "upload new webcam images"
 
 git push 
 
